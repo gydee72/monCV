3884d2a94b242b19d2ab6bf68307c57d63b278be
Ragon Géraldine <gydee72@gmail.com>
2023-03-31 19:50:30 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const txtAnim = document.querySelector('h1 .my-4');
+new Typewriter(txtAnim, {
+  loop: true,
+  deleteSpeed: 20
+})
+.changedelay(20)
+
+.typeString('Voici un aperçu')
+.pauseFor(300)
+.typeString('<strong>, de mes créations.</strong> ! ')
+.pause(1000)
+.deleteChars(10)
+.typeString('<span style="color: #27ae60">sites</span> ! ')
+.pauseFor(1000)
+.deleteChars(7)
+.typeString('<span style="color: midnightblue">sites React</span> ! ')
+.pauseFor(1000)
+.deleteChars(7)
+.typeString('<span style="color:#ea39ff">sites Wordpress</span> ! ')
+.pauseFor(1000)
+.deleteChars(9)
+
+.start()
+            
